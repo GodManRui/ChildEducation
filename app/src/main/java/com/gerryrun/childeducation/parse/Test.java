e099b242f33d42e0cd8dae8090a14981caf2b93c
BruceChou <854463112@qq.com>
2019-02-28 08:18:27 +0800

    钢琴界面适配OK

--- app/src/main/java/com/gerryrun/childeducation/parse/Test.java
+++ app/src/main/java/com/gerryrun/childeducation/parse/Test.java
@@ -2,12 +2,12 @@ package com.gerryrun.childeducation.parse;
 
 public class Test {
     public static void main(String[] args) {
-     /*   String path = "D:\\AndroidStudio\\Project\\ChildEducation\\app\\src\\main\\res\\raw\\small_start.mid ";
+        String path = "D:\\AndroidStudio\\Project\\ChildEducation\\app\\src\\main\\res\\raw\\jiequ.mid";
         ReadMIDI readMIDI = new ReadMIDI();
-        readMIDI.myRead(path, null);*/
-        String ss = "<item>@drawable/music_9_%s</item>";
+        readMIDI.myRead(path, null);
+        /*String ss = "<item>@drawable/music_9_%s</item>";
         for (int i = 0; i < 56; i++) {
             System.out.println(String.format(ss, i + 1 + ""));
-        }
+        }*/
     }
 }
