a28c1150d770165c8b6abd1999e27f542c5849aa
GodRui <zhujr@bonree.com>
2019-04-02 18:41:39 +0800

    jiezou TM OV2

--- app/src/main/java/com/gerryrun/childeducation/piano/Rhythm.java
+++ app/src/main/java/com/gerryrun/childeducation/piano/Rhythm.java
@@ -7,7 +7,6 @@ import android.media.MediaPlayer;
 import android.media.SoundPool;
 import android.os.Bundle;
 import android.os.Handler;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
@@ -16,7 +15,6 @@ import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
-import android.widget.Toast;
 
 import com.gerryrun.childeducation.piano.parse.ReadMIDI;
 import com.gerryrun.childeducation.piano.parse.entity.ResultSequence;
@@ -27,6 +25,7 @@ public class Rhythm extends BaseActivity {
 
     private View imYuePu;
     private View vIndicator;
+    private View vIndicator2;
     private ArrayList<ResultSequence> resultSequences;
     private long startPlayTimeMillis;
     //    private MediaPlayer mediaPlayer;
@@ -40,6 +39,9 @@ public class Rhythm extends BaseActivity {
     private int duration;
     private Thread playThread;
     private int shouldStop = 2;
+    private View selectView;
+//    private boolean isPlaying;
+//    private boolean firstPlay = true;
 
     public static int px2dip(float pxValue) {
         float scale = Resources.getSystem().getDisplayMetrics().density;
@@ -47,7 +49,7 @@ public class Rhythm extends BaseActivity {
     }
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rhythm);
         initView();
@@ -57,26 +59,47 @@ public class Rhythm extends BaseActivity {
     private void initView() {
         imYuePu = findViewById(R.id.im_yue_pu);
         vIndicator = findViewById(R.id.v_indicator);
+        vIndicator2 = findViewById(R.id.v_indicator2);
         imIndicator = findViewById(R.id.im_jie_pai_indicator);
         rlContorl = findViewById(R.id.rl_crontor);
+        findViewById(R.id.im_jiezou_play).setOnClickListener((v) -> {
+//            if (!firstPlay) {
+//                if (isPlaying) {
+//                    isPlaying = false;
+//                    soundPool.pause(load);
+//                } else {
+//                    isPlaying = true;
+//                    soundPool.resume(load);
+//                }
+//            } else {
+//                startPlay(duration * playRate);
+//                initAnimation();
+//                firstPlay = false;
+//            }
+        });
+        findViewById(R.id.im_ge_dan).setOnClickListener(v -> {
+            selectView.setVisibility(View.VISIBLE);
+        });
 
         findViewById(R.id.im_return).setOnClickListener(v -> {
             finish();
         });
         imYuePu.post(() -> {
-            RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) vIndicator.getLayoutParams();
+           /* RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) vIndicator.getLayoutParams();
             layoutParams.leftMargin = imYuePu.getLeft();
             layoutParams.height = imYuePu.getHeight();
             layoutParams.topMargin = imYuePu.getTop();
-            vIndicator.setLayoutParams(layoutParams);
+            vIndicator.setLayoutParams(layoutParams);*/
         });
-        findViewById(R.id.rl_select_song).setOnClickListener((v) -> {
-            v.setVisibility(View.GONE);
-            vIndicator.setVisibility(View.VISIBLE);
-            imYuePu.setVisibility(View.VISIBLE);
-            rlContorl.setVisibility(View.VISIBLE);
+        selectView = findViewById(R.id.rl_select_song);
+        selectView.setOnClickListener((v) -> {
+            v.setVisibility(v.getVisibility() == View.VISIBLE ? View.GONE : View.VISIBLE);
+//            v.setVisibility(View.GONE);
+//            vIndicator.setVisibility(View.VISIBLE);
+//            imYuePu.setVisibility(View.VISIBLE);
+//            rlContorl.setVisibility(View.VISIBLE);
 //            startPlay();
-            startPlay2();
+//            startPlay2();
         });
         findViewById(R.id.im_jiepaiqi).setOnClickListener((v) -> {
             //点击节拍器,切换快慢拍
@@ -87,6 +110,9 @@ public class Rhythm extends BaseActivity {
         });
         imPaiZi = findViewById(R.id.im_paizi);
         imPaiZi.setTag(R.drawable.music_jiepaiqi_zhongpai_1);
+
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.jiequ);
+        duration = mediaPlayer.getDuration();
     }
 
     private void initPlayer() {
@@ -101,71 +127,9 @@ public class Rhythm extends BaseActivity {
         for (ResultSequence resultSequence : this.resultSequences) {
             Log.e("jerry", "initPlayer: " + resultSequence.toString());
         }
-        soundPool = new SoundPool(8, AudioManager.STREAM_MUSIC, 100);
+        soundPool = new SoundPool(18, AudioManager.STREAM_MUSIC, 100);
         load = soundPool.load(this, R.raw.jiequ, 1);
-
-
-        Toast.makeText(this, " = " + load, Toast.LENGTH_SHORT).show();
-    }
-
-    private void startPlay2() {
-        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.jiequ);
-        duration = mediaPlayer.getDuration();
-        startPlay(duration * playRate);
-        initAnimation();
-    }
-
-    public int getNextBeatRes() {
-        if (imPaiZi == null) return R.drawable.music_jiepaiqi_zhongpai_1;
-        int currentID = (int) imPaiZi.getTag();
-        switch (currentID) {
-            case R.drawable.music_jiepaiqi_kuaipai_1:
-                playRate = 0.5f;
-                imIndicator.setImageResource(R.drawable.music_jiepaiqi_manpa);
-                return R.drawable.music_jiepaiqi_manpai_1;
-            case R.drawable.music_jiepaiqi_zhongpai_1:
-                playRate = 1.4f;
-                imIndicator.setImageResource(R.drawable.music_jiepaiqi_kuaipa);
-                return R.drawable.music_jiepaiqi_kuaipai_1;
-            case R.drawable.music_jiepaiqi_manpai_1:
-                playRate = 1f;
-                imIndicator.setImageResource(R.drawable.music_jiepaiqi_zhongpa);
-                return R.drawable.music_jiepaiqi_zhongpai_1;
-        }
-        return R.drawable.music_jiepaiqi_zhongpai_1;
-    }
-
-    private void clickStopPlay() {
-        if (playThread != null) {
-            shouldStop = 1;
-//            playThread.stop();
-            playThread.interrupt();
-            playThread = null;
-        }
-//        soundDestroy();
-        if (vIndicator == null) return;
-        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) vIndicator.getLayoutParams();
-        if (layoutParams == null) return;
-        layoutParams.leftMargin = 0;
-        vIndicator.setLayoutParams(layoutParams);
-
-       /* while (shouldStop != 2) {
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-
-        }*/
-        ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("正在切换节拍...");
-        progressDialog.setCancelable(false);
-        progressDialog.show();
-        new Handler().postDelayed(() -> {
-            progressDialog.dismiss();
-            startPlay(duration * playRate);
-            initAnimation();
-        }, 3000);
+        clickStopPlay();
     }
 
     private void startPlay(float duration) {
@@ -179,7 +143,12 @@ public class Rhythm extends BaseActivity {
             load = soundPool.play(load, 1, 1, 1, 0, playRate);
 
             startPlayTimeMillis = System.currentTimeMillis();
+
             while (resultSequences.size() > 0 && shouldStop == 0) {
+               /* if (!isPlaying) {
+                    startPlayTimeMillis = System.currentTimeMillis() - startPlayTimeMillis;
+                    continue;
+                }*/
                 float currentPlay = (System.currentTimeMillis() - startPlayTimeMillis) * playRate;
 
                 if (currentPlay > 6520 || currentPlay > duration) {
@@ -194,6 +163,9 @@ public class Rhythm extends BaseActivity {
                         LayoutParams layoutParams = (LayoutParams) vIndicator.getLayoutParams();
                         layoutParams.leftMargin = (int) marginLeft;
                         vIndicator.setLayoutParams(layoutParams);
+                        LayoutParams layoutParams2 = (LayoutParams) vIndicator2.getLayoutParams();
+                        layoutParams2.leftMargin = (int) marginLeft;
+                        vIndicator2.setLayoutParams(layoutParams2);
                     });
                     resultSequences.remove(0);
                     resultSequences.remove(0);
@@ -272,12 +244,78 @@ public class Rhythm extends BaseActivity {
 
     }
 
+    public int getNextBeatRes() {
+        if (imPaiZi == null) return R.drawable.music_jiepaiqi_zhongpai_1;
+        int currentID = (int) imPaiZi.getTag();
+        switch (currentID) {
+            case R.drawable.music_jiepaiqi_kuaipai_1:
+                playRate = 0.5f;
+                imIndicator.setImageResource(R.drawable.music_jiepaiqi_manpa);
+                return R.drawable.music_jiepaiqi_manpai_1;
+            case R.drawable.music_jiepaiqi_zhongpai_1:
+                playRate = 1.4f;
+                imIndicator.setImageResource(R.drawable.music_jiepaiqi_kuaipa);
+                return R.drawable.music_jiepaiqi_kuaipai_1;
+            case R.drawable.music_jiepaiqi_manpai_1:
+                playRate = 1f;
+                imIndicator.setImageResource(R.drawable.music_jiepaiqi_zhongpa);
+                return R.drawable.music_jiepaiqi_zhongpai_1;
+        }
+        return R.drawable.music_jiepaiqi_zhongpai_1;
+    }
+
+    private void clickStopPlay() {
+        if (playThread != null) {
+            shouldStop = 1;
+//            playThread.stop();
+            playThread.interrupt();
+            playThread = null;
+        }
+//        soundDestroy();
+        if (vIndicator == null) return;
+        if (vIndicator2 == null) return;
+        RelativeLayout.LayoutParams layoutParams = (RelativeLayout.LayoutParams) vIndicator.getLayoutParams();
+        if (layoutParams == null) return;
+        layoutParams.leftMargin = 0;
+        vIndicator.setLayoutParams(layoutParams);
+
+        RelativeLayout.LayoutParams layoutParams2 = (RelativeLayout.LayoutParams) vIndicator2.getLayoutParams();
+        if (layoutParams2 == null) return;
+        layoutParams2.leftMargin = 0;
+        vIndicator2.setLayoutParams(layoutParams2);
+
+       /* while (shouldStop != 2) {
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+        }*/
+        ProgressDialog progressDialog = new ProgressDialog(this);
+        progressDialog.setMessage("正在换拍...");
+        progressDialog.setCancelable(false);
+        progressDialog.show();
+        new Handler().postDelayed(() -> {
+            progressDialog.dismiss();
+            startPlay(duration * playRate);
+            initAnimation();
+        }, 3000);
+    }
+
     @Override
     protected void onDestroy() {
         super.onDestroy();
         soundDestroy();
         resultSequences.clear();
     }
+
+   /* private void startPlay2() {
+        MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.jiequ);
+        duration = mediaPlayer.getDuration();
+        startPlay(duration * playRate);
+        initAnimation();
+    }*/
 
     private void soundDestroy() {
         if (soundPool != null) {
